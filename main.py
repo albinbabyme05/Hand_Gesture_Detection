5f451f248133710ff4f4323adf3160efa8488bcb
Albin Baby <albinbabayme05@gmail.com>
2025-02-11 23:09:12 +0100

    Add FPS calculation to display real-time frame rate

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
+
+
+previous_time = 0
+current_time = 0
 
 while True:
     ret, frame = cap.read()
@@ -14,6 +19,13 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    # FPS Calculation
+    current_time = time.time()
+    fps = 1 / (current_time - previous_time)
+    previous_time = current_time
+
+    cv2.putText(frame, f"FPS: {int(fps)}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
 
     cv2.imshow("Hand Tracking", frame)
     if cv2.waitKey(1) & 0xFF == 27:
